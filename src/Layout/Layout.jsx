6d49e954cae6eba56e9b201ce8a378a497145822
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-07-29 15:48:23 +0300

    Update Layout.jsx

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,22 +2,26 @@ import Header from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { ColorRing } from 'react-loader-spinner';
+import { LoaderWrapper } from '../components/commonCss.styled';
 
 const Layout = () => {
   return (
     <>
       <Header />
+
       <Suspense
         fallback={
-          <ColorRing
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass="blocks-wrapper"
-            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-          />
+          <LoaderWrapper>
+            <ColorRing
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="blocks-loading"
+              wrapperStyle={{}}
+              wrapperClass="blocks-wrapper"
+              colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            />
+          </LoaderWrapper>
         }
       >
         <Outlet />
